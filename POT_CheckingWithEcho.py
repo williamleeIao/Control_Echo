0198c8acc2298dd91cb1603b3d44b1c037ca94c9
WilliamleeIao <Wilee_6@hotmail.com>
2020-10-02 14:21:04 +0800

    Finalize Code

--- POT_CheckingWithEcho.py
+++ POT_CheckingWithEcho.py
@@ -1,10 +1,46 @@
 from selenium import webdriver
 from time import sleep, time
 from bs4 import BeautifulSoup
-import string
+import openpyxl
+import re
 from bs4.element import NavigableString, Tag
 from datetime import datetime, date
-import os
+import functools
+
+
+file_name = r"C:\Users\willlee\Downloads\LIST POT 2020.xlsx"
+
+
+class Excel_Operation:
+
+    def replace_alphabet(self, text):
+        return re.sub(r'[a-zA-Z]', r'*', text)
+
+    def __init__(self, file_name, sheet_name):
+        self.file_name = file_name
+        self.sheet_name = sheet_name
+
+    def load_workbook_with_sheet_name(self):
+        self.book = openpyxl.load_workbook(self.file_name)
+        self.work_sheet = self.book[self.sheet_name]
+
+    def cell_to_read(self, column_to_read, row_to_start):
+        # This function is assumed both read and write having the same row thus just have one row_to_start variable
+        read_position = column_to_read + row_to_start  # C2
+
+        text = self.work_sheet[read_position].value
+        new_string = self.replace_alphabet(text)
+        # replace last character back
+        new_string = new_string[:-1] + text[-1]
+        print(new_string)
+        return new_string, text
+
+    def cell_to_write(self, column_to_write, row_to_start, value_to_write_in):
+        write_position = column_to_write + row_to_start  # D2
+        self.work_sheet[write_position].value = value_to_write_in
+
+    def save_file(self, file_name):
+        self.book.save(file_name)
 
 
 class webpage_Extract:
@@ -37,7 +73,7 @@ class webpage_Extract:
                     oneD_array.append(items[0].contents[item].contents[individual].text)
                 twoD_array.append(oneD_array)
             # reset back 1D array
-            oneD_array=[]
+            oneD_array = []
         return twoD_array
 
 
@@ -74,11 +110,38 @@ class Control_Echo:
         print("get _driver ")
         return self.driver
 
+    def teardown(self):
+        self.driver.close()
+        self.driver.quit()
+        print("Finished")
 
+# put an decorator for time recording
+def logging_time(func):
+    @functools.wraps(func)
+    def wrapper_time(*args):
+        start_time = time()
+        func(*args)
+        end_time = time()
+        run_time = end_time - start_time
+        print("--- %s seconds ---" % (time() - start_time))
+
+    return wrapper_time
+
+@logging_time
 def main():
     successfully = False
+    # ----------Webpage Operation-------------------------------
     webpage_ext = webpage_Extract()
+    # ---------Excel Operation---------------------------------
+    column_to_read = "C"
+    column_to_write = "D"
+    row_to_start = "339"
+    row_to_end =""
+    excel_operation = Excel_Operation(file_name, r"LIST POT 2020")
+    excel_operation.load_workbook_with_sheet_name()
+    # ---------Webpage Operation-------------------------------
     echo = Control_Echo()  # init webdriver
+    # ---------------------------------------------------------
     echo.launch_address('https://echo.natinst.com/')
     echo.buffer_time(10)
     print('opened Echo')
@@ -99,23 +162,56 @@ def main():
     input("Once Enter , press any key to continue")
     echo.buffer_time(10)
 
-    successfully = echo.webpage_control(id_name='inputPart', action='write',
-                                        value='159572*-000L')
-    if successfully:
-        print("Part Number Entered")
-        successfully = False
-    echo.buffer_time(5)
+    while True:
+        print (row_to_start)
+        wild_char_text, text = excel_operation.cell_to_read(column_to_read, row_to_start)
+        if text == "":
+            break
+        else:
 
-    successfully = echo.webpage_control(id_name='searchButton', action='click')
-    if successfully:
-        print("Search button Clicked")
-        successfully = False
+            successfully = echo.webpage_control(id_name='inputPart', action='write',
+                                                value=wild_char_text)
+            if successfully:
+                print("Part Number Entered")
+                successfully = False
+            echo.buffer_time(3)
 
-    echo.buffer_time(5)
+            successfully = echo.webpage_control(id_name='searchButton', action='click')
+            if successfully:
+                print("Search button Clicked")
+                successfully = False
 
-    # driver = echo.get_driver()
-    soup = webpage_ext.webpage_refresh(echo.driver)
-    webpage_ext.check_all(0, 5)
+            echo.buffer_time(4)
+
+            # driver = echo.get_driver()
+            soup = webpage_ext.webpage_refresh(echo.driver)
+            two_d_list = webpage_ext.check_all(0, 5)
+
+            # do what? if it look for most active Check if active same with the text need to take part number to check whether it is active
+            for i in two_d_list:
+                if i[0] == text:  # word might not correct
+                    # take out the active one and do comparison with the excel if same , then write active if not same then write latest one
+                    if i[1] == "Active":
+                        excel_operation.cell_to_write(column_to_write, row_to_start, "Active")
+
+                        break
+                else:  # this is not active one
+                    # look for the active
+                    if i[1] == "Active" or i[1] == "Standard Support":
+                        if i[1] == "Active":
+                            statement = "Active part number is " + i[0]
+                        if i[1] == "Standard Support":
+                            statement = "Standard Support part number is " + i[0]
+                        excel_operation.cell_to_write(column_to_write, row_to_start, statement)
+
+            successfully = echo.webpage_control(id_name='clearSearchButton', action='click')
+            if successfully:
+                print("Clear button Clicked")
+                row_to_start = int(row_to_start) + 1
+                row_to_start = str(row_to_start)
+                excel_operation.save_file(r"C:\Users\willlee\Downloads\LIST POT 2020_2.xlsx") #Save content
+                successfully = False
+    echo.teardown()
 
 
 if __name__ == "__main__":
